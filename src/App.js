ae667889ae93968a15cab33cda7497913124b2e7
Jinok <joseo46@hotmail.com>
2021-12-14 14:16:26 +1100

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <Navbar bg='dark' variant='dark' expand='md'>
+        <Container>
+          <Navbar.Brand href='#'>COSMOS</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Item>
+                <Nav.Link eventKey='1' href='#/home'>
+                  Spacecraft
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey='2' title='Item'>
+                  Astronauts
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey='3' disabled>
+                  News
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default App;
