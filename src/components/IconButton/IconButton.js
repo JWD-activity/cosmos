9d5c742229f0bf99d014270c2dda63b9b898bc0a
Jinok <joseo46@hotmail.com>
2021-12-16 15:01:57 +1100

    add details structure

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BsFillArrowRightSquareFill } from 'react-icons/bs';
 import { BsFillArrowLeftSquareFill } from 'react-icons/bs';
-import { BsCardList } from 'react-icons/bs';
+import { BsFillFileEarmarkTextFill } from 'react-icons/bs';
 import './IconButton.css';
 
 function IconButton({ type }) {
   let icon;
   if (type === 'next') icon = <BsFillArrowRightSquareFill />;
   if (type === 'prev') icon = <BsFillArrowLeftSquareFill />;
-  if (type === 'goback') icon = <BsCardList />;
+  if (type === 'goback') icon = <span className='btn-goback'>GO BACK</span>;
 
   return (
     <button className='btn-icon' type='button'>
--- src/screen/SpacecraftDetails/SpacecraftDetails.js
+++ src/screen/SpacecraftDetails/SpacecraftDetails.js
@@ -3,6 +3,8 @@ import './SpacecraftDetails.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import Image from 'react-bootstrap/Image';
 import IconButton from '../../components/IconButton/IconButton';
 import './SpacecraftDetails.css';
 
@@ -11,19 +13,38 @@ function SpacecraftDetails() {
     <main className='position-relative'>
       <Container className='spaceDetails-container'>
         <Row>
-          <Col className='col-btn text-start'>
+          <Col className='col-btn text-start d-flex align-items-center'>
             <IconButton type='prev' />
           </Col>
 
           <Col>
-            <Row className='flex-column'>
-              <Col>1</Col>
-              <Col>2</Col>
-              <Col>3</Col>
-              <Col>4</Col>
+            <Row className='flex-column gap-3 p-5'>
+              <Col>
+                <IconButton type='goback' />
+              </Col>
+              <Col>
+                <h2>Title: Heading 1</h2>
+              </Col>
+              <Col>
+                <Image src='../../assets/news.jpg' fluid />
+              </Col>
+              <Col>
+                <Badge bg='primary'>Primary</Badge>{' '}
+                <Badge bg='secondary'>Secondary</Badge>{' '}
+                <Badge bg='success'>Success</Badge>{' '}
+                <Badge bg='danger'>Danger</Badge>{' '}
+                <Badge bg='warning' text='dark'>
+                  Warning
+                </Badge>
+              </Col>
+              <Col>
+                <h3>Title: Heading 2</h3>
+                <p>content</p>
+              </Col>
             </Row>
           </Col>
-          <Col className='col-btn text-end'>
+
+          <Col className='col-btn text-end d-flex align-items-center'>
             <IconButton type='next' />
           </Col>
         </Row>
