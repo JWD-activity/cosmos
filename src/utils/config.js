087a6e8fdd10bd4c7ff50ba913d84157a3116b27
Jinok <joseo46@hotmail.com>
2021-12-20 21:19:12 +1100

    modify link

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-// export const PUBLIC = process.env.PUBLIC_URL;
+export const PUBLIC = process.env.PUBLIC_URL;
 export const URL_BASE = 'https://lldev.thespacedevs.com/2.2.0/';
 export const GET_ALL_SPACECRAFT = 'config/spacecraft/';
 export const DETAILS = 'spacecraft/';
