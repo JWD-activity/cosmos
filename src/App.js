1234bcbc4c476ef29e9db76f122d07e125a5a347
Jinok <joseo46@hotmail.com>
2021-12-21 07:20:09 +1100

    modify router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './screen/Home/Home';
 import Spacecraft from './screen/Spacecraft/Spacecraft';
