0be656208921bcb5dea2cc423002ca30ddbe83c9
Jinok <joseo46@hotmail.com>
2021-12-20 22:05:29 +1100

    modify link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { PUBLIC } from './utils/config';
 import Home from './screen/Home/Home';
 import Spacecraft from './screen/Spacecraft/Spacecraft';
 import SpacecraftDetails from './screen/SpacecraftDetails/SpacecraftDetails';
@@ -18,8 +19,11 @@ function App() {
         <Navigation />
         <Main>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='spacecraft' element={<Spacecraft />} />
+            <Route path={PUBLIC + '/'} element={<Home />} />
+            <Route
+              path={process.env.PUBLIC_URL + 'spacecraft'}
+              element={<Spacecraft />}
+            />
             <Route path='spacecraft/:id' element={<SpacecraftDetails />} />
             <Route path='astronauts' element={<Astronauts />} />
             <Route path='astronauts/:id' element={<AstronautDetails />} />
